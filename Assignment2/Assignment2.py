85b87da5bf5c4024750b2f41f4ebc749d8eefc72
Sean Nishi <Sean-Nishi@users.noreply.github.com>
2020-02-05 12:49:51 -0500

    Add files via upload

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -13,28 +13,28 @@ GPIO.setup(18, GPIO.OUT)
 
 buttonState = False
 led = False
-changed = False
+clicked = False
 
 while True:
     #is the button pressed?
     buttonState = GPIO.input(21)
 
-    #if button is pressed, have we already changed the light?
-    if buttonState == False:
-        changed = False
+    #if the button is clicked and it wasn't previously clicked
+    if (buttonState and not clicked):
+        clicked = True
 
     #if we havent change the light
-    if (changed == False and buttonState == True):
+    if (not buttonState and clicked):
+        clicked = False
+        
         #and if the button is pressed and the light is on
         #set light to on and save the state
         if (led == True):
             GPIO.output(18, GPIO.LOW)
             led = False
-            changed = True
 
         #else if the button is pressed and the light is off
         #turn the light on and save the state
         elif (led == False):
             GPIO.output(18, GPIO.HIGH)
             led = True
-            changed = True
